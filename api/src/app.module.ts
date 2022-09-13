71f646460eebca7133aff99c89fd53820f21f16f
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-13 04:12:06 -0300

    read.me y more bases

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,14 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      })
+    ,UsersModule],
   controllers: [],
   providers: [],
 })
