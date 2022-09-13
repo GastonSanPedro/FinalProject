96d644dcac3ec99d115d2adcc85968843a179b92
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-13 14:38:38 -0300

    first routes

---
+++ api/src/posteos/posteos.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PosteosService } from './posteos.service';
+import { CreatePosteoDto } from './dto/create-posteo.dto';
+import { UpdatePosteoDto } from './dto/update-posteo.dto';
+
+@Controller('posteos')
+export class PosteosController {
+  constructor(private readonly posteosService: PosteosService) {}
+
+  @Post()
+  create(@Body() createPosteoDto: CreatePosteoDto) {
+    return this.posteosService.create(createPosteoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.posteosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.posteosService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePosteoDto: UpdatePosteoDto) {
+    return this.posteosService.update(+id, updatePosteoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.posteosService.remove(+id);
+  }
+}
