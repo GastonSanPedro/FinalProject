2338b1ad77bb7f6fee1e16cbc05496798f12cff5
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-14 21:01:58 -0300

    db connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,13 +3,17 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { UsersModule } from './users/users.module';
 import { PosteosModule } from './posteos/posteos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
-      })
-    ,UsersModule, PosteosModule],
+      }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-FinalProject'),
+    UsersModule, 
+    PosteosModule
+  ],
   controllers: [],
   providers: [],
 })
