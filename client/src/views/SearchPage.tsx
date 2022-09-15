52dc0ab25e9334a241c67313bbb392052cd1c4ef
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-09-15 20:50:04 -0300

    Searchbar y navbar

--- client/src/views/SearchPage.tsx
+++ client/src/views/SearchPage.tsx
@@ -2,11 +2,12 @@ import { Box } from "@chakra-ui/react";
 import React from "react";
 import HashtagContainer from "../components/HashtagSearch/HashtagContainer";
 import UserSearchContainer from "../components/UserSearch/UserSearchContainer";
+import Navbar from '../components/navbar/Navbar'
 
 const SearchPage = () =>{
     return(
         <>
-        <Box backgroundColor='green.200'>Esto es el nav</Box>
+        <Navbar/>
         <UserSearchContainer/>
         <HashtagContainer/>
         </>
