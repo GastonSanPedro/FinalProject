7f7de6c68cfe30023f31747ea5cc696d2dcd0d27
saltorodrigo <rodrigosalto95@gmail.com>
2022-09-15 19:23:59 -0300

    creacion de vista feed

---
+++ client/src/views/Feed.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserSinglePost from '../components/UserSinglePost/UserSinglePost'
+
+const Feed = () => {
+
+    return (
+        <>
+            <UserSinglePost />
+            <UserSinglePost />
+        </>
+    )
+}
+
+export default Feed
