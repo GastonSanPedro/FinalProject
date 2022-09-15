7cd84b8a11b943f8be443920d43096df841f5ca7
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-09-15 18:45:41 -0300

    reducer, action

--- client/src/redux/actions.tsx
+++ client/src/redux/actions.tsx
@@ -1,1 +1,15 @@
-export {}
+import axios from 'axios';
+
+export function getUsers(){
+    return async function(dispatch:Function){
+        try{
+            var json = await axios.get('hhtp://localhost:3001/users')
+            return dispatch({
+                type: 'GET_USERS',
+                payload: json.data
+            })
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
