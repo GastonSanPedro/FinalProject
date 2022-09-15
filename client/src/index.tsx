489f5f6720689d9b7dc708c3225a34f775d8c1f6
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-09-15 18:50:11 -0300

    store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,11 +10,14 @@ import SignIn from './views/SignIn'
 import { ChakraProvider } from '@chakra-ui/react'
 import SearchPage from './views/SearchPage';
 import theme from './theme';
+import {Provider} from 'react-redux';
+import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <Provider store={store}>
   <React.StrictMode>
     <ChakraProvider theme={theme}>
     <BrowserRouter>
@@ -29,6 +32,7 @@ root.render(
 
     </ChakraProvider>
   </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
