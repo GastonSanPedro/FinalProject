554d70485b0a9797a1d0c9a599cf76b11444dc5b
saltorodrigo <rodrigosalto95@gmail.com>
2022-09-16 14:01:52 -0300

    Vista feed creada

--- client/src/views/Feed.tsx
+++ client/src/views/Feed.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import UserSinglePost from '../components/UserSinglePost/UserSinglePost'
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsers } from '../redux/actions';
+import { State } from "../redux/reducers"
+import { useAppDispatch } from '../redux/hooks';
+
 
 const Feed = () => {
 
+    const dispatch = useAppDispatch()
+    // dispatch(getUsers())
+
+    const users = useSelector((state: State) => state.users);
+
+
+    
+
+
+    // useEffect(() => {
+    //     dispatch(getUsers())
+    // }, [dispatch])
+
     return (
         <>
+            {users.map}
+
             <UserSinglePost />
             <UserSinglePost />
         </>
