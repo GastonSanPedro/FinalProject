0c7060b8eabe00d037b8ed640dc6f7991dcea8d6
El Bebelo <nacho71197@gmail.com>
2022-09-16 19:06:47 -0300

    Agrego la Nav de Ari a la view Profile

--- client/src/views/Profile.tsx
+++ client/src/views/Profile.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CreatePost from '../components/CreatePost/CreatePost';
+import Navbar from '../components/navbar/Navbar';
 import ProfileDetail from '../components/ProfileDetail/ProfileDetail';
 import UserPost from '../components/UserPosts/UserPost';
 import "../index.css";
@@ -7,7 +8,7 @@ import "../index.css";
 const Profile = () => {
   return (
     <>
-        <nav style={{ backgroundColor:'green'}} >soy la nav, pa!</nav>
+        <Navbar></Navbar>
         <ProfileDetail/>
         <CreatePost/>
         <UserPost/>
