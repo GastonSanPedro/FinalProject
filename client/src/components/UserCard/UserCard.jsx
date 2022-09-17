a9c3fb25ee0330ae2ed9cbf8c5ee5a0f830acecc
El Bebelo <nacho71197@gmail.com>
2022-09-17 18:47:17 -0300

    Auth falso, y renderizado de datos del usuario por localStorage

---
+++ client/src/components/UserCard/UserCard.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Stack, Avatar, Text, Button } from '@chakra-ui/react';
+
+const userImg: string =
+  'https://previews.123rf.com/images/pandavector/pandavector1901/pandavector190105171/126078877-vector-design-of-avatar-and-dummy-symbol-set-of-avatar-and-image-stock-vector-illustration-.jpg?fj=1';
+
+const UserCard = () => {
+  const [User, setUser] = useState(JSON.parse(localStorage.getItem('user')));
+  //console.log(User);
+  return (
+    <>
+      <Stack
+        m={3}
+        ml={0}
+        display="flex"
+        flexDir="column"
+        w="200px"
+        h="245px"
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="gray.200"
+        borderRadius="7px"
+      >
+        <Avatar size="xl" name="user" src={userImg} />
+        <Text as="b" fontSize="sm">
+          {User.firstName + User.lastName}
+        </Text>
+        <Button size="sm">Log Out</Button>
+      </Stack>
+    </>
+  );
+};
+
+export default UserCard;
