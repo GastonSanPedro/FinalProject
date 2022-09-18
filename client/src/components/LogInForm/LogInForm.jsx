7c41ff53d162907e84093e4c0fb1dc3f6858ef4a
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-18 02:10:01 -0300

    fix auth

--- client/src/components/LogInForm/LogInForm.jsx
+++ client/src/components/LogInForm/LogInForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import {
   Tooltip,
@@ -29,15 +29,31 @@ const CreateUser = () => {
   const handleInputChange = (event) =>
     setInput({ ...input, [event.target.name]: event.target.value });
   const isError = input === ''; //true or false
+  const [isValidate, setIsValidate]= React.useState(false)
+//   const mounted = useRef();
+// useEffect(() => {
+//   if (!mounted.current) {
+//     // do componentDidMount logic
+//     mounted.current = true;
+//   } else {
+//     // do componentDidUpdate logic
+//   }
+// });
+
+  useEffect(()=>{
+    isUserValidate()
+  },[auth])
 
   const handleSubmit = (input) => {
     dispatch(authUser(input.email, input.pass));
-    console.log(auth);
-    if (auth.auth === true) {
-      localStorage.setItem('user', JSON.stringify(auth.user.data));
-    }
-    navigate(`/profile`);
   };
+
+  const isUserValidate = ()=>{
+    if(auth.auth){
+      localStorage.setItem('user', JSON.stringify(auth.user.data));
+      navigate(`/profile`);
+    }
+  }
 
   return (
     <>
