8bb983e03bdeefe9a96cd445b9d3e627d4a90959
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-18 18:57:49 -0300

    property posteos in dto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Iposteos } from 'src/seed/interfaces/user-response.interface';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    posteos?: Iposteos[]
+}
