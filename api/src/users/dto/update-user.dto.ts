7b91c4077be1cff2d0bfe2e47de2a4a3d1c44ff5
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-18 19:20:13 -0300

    add validator to updateuserdto

--- api/src/users/dto/update-user.dto.ts
+++ api/src/users/dto/update-user.dto.ts
@@ -1,7 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsNotEmpty } from 'class-validator';
 import { Iposteos } from 'src/seed/interfaces/user-response.interface';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsNotEmpty()
     posteos?: Iposteos[]
 }
