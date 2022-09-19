ae7ed9bbb02290932471a2a6d56fe1a22ec169aa
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-09-19 03:51:51 -0300

    vistas arregladas

--- client/src/components/Friends/FriendsContainer.jsx
+++ client/src/components/Friends/FriendsContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading, Stack } from "@chakra-ui/react";
+import { Text, Stack } from "@chakra-ui/react";
 import Friend from "./Friend";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
@@ -23,8 +23,8 @@ const FriendsContainer = () => {
     return (
         <>
         {allUsers.length >1 ?
-            <Stack w='230px' minH='200px' backgroundColor='gray.300' borderRadius='7px' alignItems='center' pt={2} pb={2}>
-                <Heading as="h3" size='md' mb="5" mt="5" color="#606c38"> Contactos:</Heading>
+            <Stack justifyItems={'center'} w='230px' minH='200px' backgroundColor='gray.300' borderRadius='7px' alignItems='center' pt={2} pb={2}>
+                <Text as="h3" size='sm' mb="5" mt="5">Friends</Text>
                 {allUsers && allUsers.map((e) => {
                     return (
                         <Friend
--- client/src/views/Feed.jsx
+++ client/src/views/Feed.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import UserSinglePost from '../components/UserSinglePost/UserSinglePost'
+import PostSearch  from '../components/PostSearch/PostSearch'
 import Navbar from '../components/navbar/Navbar';
 import CreatePost from '../components/CreatePost/CreatePost';
-import { HStack, VStack } from '@chakra-ui/react'
+import { HStack, VStack,Wrap, Box, Text } from '@chakra-ui/react'
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts, getUsers } from '../redux/actions';
@@ -10,43 +10,68 @@ import FriendsContainer from '../components/Friends/FriendsContainer';
 import UserCard from '../components/UserCard/UserCard';
 
 
-// let users =[]
-
-
-
 const Feed = () => {
 
     const dispatch = useDispatch()
 
+    const users = useSelector((state) => state.users)
     const allUsers = useSelector((state) => state.users)
-    const allPosts = useSelector((state) => state.posts)
 
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [dispatch])
+    const posteosUser = users?.map( user =>{
+      return(  
+          {
+              fullName: user.fullName,
+              image: user.image,
+              posteos: user.posteos.map(posteo => posteo.description)
+          }
+      )})
+
+    const post = posteosUser.map( user => {
+        if( user.posteos.some(post => post.includes(' '))) {
+            return ({
+                fullName: user.fullName,
+                image: user.image,
+                post: user.posteos.find(post => post.includes(' '))
+            
+        })}
+    })
+
     useEffect(() => {
         dispatch(getUsers())
     }, [dispatch])
 
     return (
         <>
-
-            <Navbar />
-            <HStack >
-                <VStack>
-                    <CreatePost />
-                    <HStack >
-                        
-                    </HStack>
-                </VStack>
-                <VStack>
-                    <UserCard />
-                    {allUsers.length > 1 ?
-                    <FriendsContainer
-                    allUsers = {allUsers}
-                     /> : null}
-                </VStack>
-            </HStack>
+        <Navbar />
+        <Box display={'flex'} dir={'row'} >
+        <Box m={3}>
+            <CreatePost />
+            <Wrap justify={'center'} spacing={30} w='980px' borderRadius='7px'p={8} m={3} backgroundColor={"gray.300"}>
+                  {
+                    post
+                    ? post.map( user => {
+                      if(user?.fullName && user?.post){
+                        return(
+                          <PostSearch
+                            fullName={user?.fullName}
+                            image={user?.image}
+                            posteos={user?.post}/>
+                    )
+               }
+            })
+            : <Box><Text>no hay posteos</Text> </Box>
+        }            
+            </Wrap>
+        </Box>
+        <Box>
+        <UserCard />
+          {allUsers.length > 1 ?
+            <FriendsContainer
+              allUsers = {allUsers}
+              /> : null}
+        </Box>
+        </Box>
+               
         </>
     )
 }
