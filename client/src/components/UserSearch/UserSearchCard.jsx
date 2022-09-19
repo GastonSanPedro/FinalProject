f797a76e640c073e55a4a83e1589997142414784
AritaRuv <ariadnaruvini@hotmail.com.ar>
2022-09-19 11:49:24 -0300

    link en search page

--- client/src/components/UserSearch/UserSearchCard.jsx
+++ client/src/components/UserSearch/UserSearchCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Avatar, Text, Stack, Box } from "@chakra-ui/react";
 
 const userImg = 'https://previews.123rf.com/images/pandavector/pandavector1901/pandavector190105171/126078877-vector-design-of-avatar-and-dummy-symbol-set-of-avatar-and-image-stock-vector-illustration-.jpg?fj=1'
 
 
-const UserSearchCard = ({firstName, lastName, image}) =>{
+const UserSearchCard = ({firstName, lastName, image, email}) =>{
     return(
         <>
+        <Link to={`/user/${email}`}>
         <Stack display='flex' flexDir='column' w='100px' h='150px' alignItems='center' justifyContent='center' backgroundColor='gray.200' borderRadius='7px' >
             <Box>
             <Avatar
@@ -18,9 +20,8 @@ const UserSearchCard = ({firstName, lastName, image}) =>{
             <Box textAlign='center'>
                 <Text as='b' fontSize='xs'>{firstName} {lastName}</Text>
             </Box>
-            
-
         </Stack>
+        </Link>
         </>
     )
 }
