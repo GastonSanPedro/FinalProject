4887d7b252ceeacac01e23934732bd5d6210910d
saltorodrigo <rodrigosalto95@gmail.com>
2022-09-19 14:52:41 -0300

    nada en particular

--- client/src/views/Feed.jsx
+++ client/src/views/Feed.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import PostSearch  from '../components/PostSearch/PostSearch'
 import Navbar from '../components/navbar/Navbar';
 import CreatePost from '../components/CreatePost/CreatePost';
-import { HStack, VStack,Wrap, Box, Text } from '@chakra-ui/react'
+import { HStack, VStack,Wrap, Box, Text, Button } from '@chakra-ui/react'
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts, getUsers } from '../redux/actions';
@@ -40,6 +40,34 @@ const Feed = () => {
         dispatch(getUsers())
     }, [dispatch])
 
+
+    //--------- Lógica de ver mas --------
+    // const [currentStart, setCurrentStart] = useState(0)
+    // const [currentEnd, setCurrentEnd] = useState(8)
+    // const [button1, setButton1] = useState(true)
+    // const [button2, setButton2] = useState(false)
+    // const first8 = filtered.length > 8 ? filtered?.slice(currentStart, currentEnd) : filtered
+
+
+    // const handleClickFoward = () => {
+
+    //     if (currentStart >= 0) setButton1(false)
+
+    //     setCurrentStart(currentStart + 8)
+    //     setCurrentEnd(currentEnd + 8)
+    // }
+
+    // const handleClickBackward = () => {
+
+    //     if (currentStart <= 1) setButton1(true)
+    //     setCurrentStart(currentStart - 8)
+    //     setCurrentEnd(currentEnd - 8)
+    // }
+    //--------------------------------------
+
+
+
+
     return (
         <>
         <Navbar />
@@ -47,6 +75,7 @@ const Feed = () => {
         <Box m={3}>
             <CreatePost />
             <Wrap justify={'center'} spacing={30} w='980px' borderRadius='7px'p={8} m={3} backgroundColor={"gray.300"}>
+            {/* <Box mr="300"><Button >Ver más</Button></Box> */}
                   {
                     post
                     ? post.map( user => {
@@ -60,7 +89,12 @@ const Feed = () => {
                }
             })
             : <Box><Text>no hay posteos</Text> </Box>
-        }            
+        }     
+
+        
+
+
+
             </Wrap>
         </Box>
         <Box>
