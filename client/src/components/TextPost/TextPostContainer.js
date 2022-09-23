1cae530f2b50e1d259e8b7f68750531b68076f86
El Bebelo <nacho71197@gmail.com>
2022-09-23 15:35:55 -0300

    arreglo del post y de los posteos que se renderizan (Parcialmente mientra no hay relaciones)

--- client/src/components/TextPost/TextPostContainer.js
+++ client/src/components/TextPost/TextPostContainer.js
@@ -12,7 +12,7 @@ import {
 import TextPost from './TextPost';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPosts, getUsers } from '../../redux/action';
+import { getPosts, getUsers, getUser } from '../../redux/action';
 import CreatePost from '../CreatePost/CreatePost';
 import UserSearchContainer from '../UserSearch/UserSearchContainer';
 
@@ -32,6 +32,8 @@ export default function TextPostContainer({ site, word, email }) {
   const dispatch = useDispatch();
   const { isOpen, onToggle } = useDisclosure();
   const users = useSelector((state) => state.users);
+  const user = useSelector((state) => state.user);
+  const myUser = useSelector((state) => state.myUser);
   const posteosUser = users?.map((user) => {
     return {
       fullName: user.fullName,
@@ -56,6 +58,7 @@ export default function TextPostContainer({ site, word, email }) {
 
   useEffect(() => {
     dispatch(getUsers());
+    dispatch(getUser(email));
   }, [dispatch]);
 
   //--------- Lógica de ver mas --------
@@ -87,6 +90,61 @@ export default function TextPostContainer({ site, word, email }) {
           mt={'40vh'}
           mr={5}
         >
+          {user ? (
+            user?.posteos?.map((p, index) => {
+              return (
+                <SlideFade in={onToggle} offsetY="20px">
+                  <Box key={index}>
+                    <TextPost
+                      fullName={user?.fullName}
+                      image={user?.image}
+                      description={p.description}
+                      background={`logo.${Math.random(1, 2, 3)}`}
+                      id={index}
+                    />
+                  </Box>
+                </SlideFade>
+              );
+            })
+          ) : (
+            <Box>
+              <Text>no hay posteos</Text>{' '}
+            </Box>
+          )}
+        </SimpleGrid>
+        <Center>
+          <Button
+            onClick={() => handleClickMore()}
+            h="50px"
+            w="200px"
+            mr="50"
+            fontSize="sm"
+            mt="50px"
+            mb="50px"
+          >
+            Ver más
+          </Button>
+        </Center>
+      </Flex>
+    );
+  } else if (site === 'search' || site === 'feed') {
+    return (
+      <Flex
+        pr={'2%'}
+        pl={'2%'}
+        textAlign={'center'}
+        justifyContent={'center'}
+        direction={'column'}
+        bg={'rgba(229, 191, 124, 0.2)'}
+        borderRadius={2}
+        mt={site === 'feed' ? '0vh' : '4vh'}
+      >
+        {site === 'search' ? (
+          <UserSearchContainer word={word} />
+        ) : (
+          <CreatePost site={site} email={email} />
+        )}
+        <SimpleGrid columns={{ base: 1, xl: 2 }} spacing={'10'} mt={2} mr={5}>
           {post ? (
             renderPosts.map((user, index) => {
               if (user?.fullName && user?.post) {
@@ -138,29 +196,23 @@ export default function TextPostContainer({ site, word, email }) {
         borderRadius={2}
         mt={site === 'feed' ? '0vh' : '4vh'}
       >
-        {site === 'search' ? (
-          <UserSearchContainer word={word} />
-        ) : (
-          <CreatePost site={site} email={email} />
-        )}
+        <CreatePost site={site} email={email} />
         <SimpleGrid columns={{ base: 1, xl: 2 }} spacing={'10'} mt={2} mr={5}>
-          {post ? (
-            renderPosts.map((user, index) => {
-              if (user?.fullName && user?.post) {
-                return (
-                  <SlideFade in={onToggle} offsetY="20px">
-                    <Box key={index}>
-                      <TextPost
-                        fullName={user?.fullName}
-                        image={user?.image}
-                        description={user?.post}
-                        background={`logo.${Math.random(1, 2, 3)}`}
-                        id={index}
-                      />
-                    </Box>
-                  </SlideFade>
-                );
-              }
+          {myUser ? (
+            myUser?.posteos?.map((p, index) => {
+              return (
+                <SlideFade in={onToggle} offsetY="20px">
+                  <Box key={index}>
+                    <TextPost
+                      fullName={myUser?.fullName}
+                      image={myUser?.image}
+                      description={p.description}
+                      background={`logo.${Math.random(1, 2, 3)}`}
+                      id={index}
+                    />
+                  </Box>
+                </SlideFade>
+              );
             })
           ) : (
             <Box>
