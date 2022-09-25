06384742bce1ca90f33dc6fd3b9c1139f09ca9b7
El Bebelo <nacho71197@gmail.com>
2022-09-24 21:55:20 -0300

    Agrego botones para chat

--- client/src/components/Friends/FriendCard.jsx
+++ client/src/components/Friends/FriendCard.jsx
@@ -1,39 +1,41 @@
-import { Link, Flex, Avatar, Text  } from "@chakra-ui/react";
-
+import { Link, Flex, Avatar, Text, Button } from '@chakra-ui/react';
+import { BiMessageDetail } from 'react-icons/bi';
 const userImg =
   'https://previews.123rf.com/images/pandavector/pandavector1901/pandavector190105171/126078877-vector-design-of-avatar-and-dummy-symbol-set-of-avatar-and-image-stock-vector-illustration-.jpg?fj=1';
 
-
-export const FriendCard = ({ image, link,firstName, lastName, email }) => {
-    return (
-      <Link
-        href={'/user/' + email}
-        style={{ textDecoration: 'none' }}
-        _focus={{ boxShadow: 'none' }}
+export const FriendCard = ({ image, link, firstName, lastName, email }) => {
+  return (
+    <Link
+      href={'/user/' + email}
+      style={{ textDecoration: 'none' }}
+      _focus={{ boxShadow: 'none' }}
+    >
+      <Flex
+        align="center"
+        w={'100%'}
+        p="4"
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        _hover={{
+          bg: '#8ea26f',
+          color: 'white',
+        }}
       >
-        <Flex
-          align="center"
-          w={'100%'}
-          p="4"
-          
-          borderRadius="lg"
-          role="group"
-          cursor="pointer"
-          _hover={{
-            bg: '#8ea26f',
-            color: 'white',
-          }}
+        <Avatar size={'sm'} name="user" src={image ? image : userImg} />
+        <Text pl={2} fontSize={'1vw'} fontWeight={'bold'}>
+          {firstName[0].toUpperCase() + firstName.substring(1)}{' '}
+          {lastName[0].toUpperCase() + lastName.substring(1)}
+        </Text>
+        <Button
+          position={'absolute'}
+          right={'15%'}
+          bg={'orange.200'}
+          _hover={{ bg: 'orange', color: 'black' }}
         >
-        <Avatar
-            size={'sm'}
-            name="user"
-            src={image? image : userImg}
-            />
-            <Text pl={2} fontSize={'1vw'} fontWeight={'bold'}> 
-            {firstName[0].toUpperCase()+ firstName.substring(1)} {lastName[0].toUpperCase()+ lastName.substring(1)}
-            </Text>      
-          
-        </Flex>
-      </Link>
-    );
-  };
+          <BiMessageDetail color="black" />
+        </Button>
+      </Flex>
+    </Link>
+  );
+};
