c251f45fe00827b810e5ad40b4281991de4648c1
Ezequiel Etchevest <e.etchevest@hotmail.com>
2022-09-26 10:31:43 -0300

    :

--- api/src/seed/seed.module.ts
+++ api/src/seed/seed.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { UsersModule } from '../users/users.module';
+import { PosteosModule } from 'src/posts/post.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
   imports: [
-    UsersModule
+    UsersModule,
+    PosteosModule
   ]
 })
 export class SeedModule {}
