265b0d442ac36fbc2a0fe22f0b75a2ba931481ca
gastonemanuel1997@hotmail.com <gastonemanuel1997@hotmail.com>
2022-09-28 21:11:51 -0300

    asd

--- client/src/components/ImgPost/ImgPostContainer.jsx
+++ client/src/components/ImgPost/ImgPostContainer.jsx
@@ -5,25 +5,36 @@ import {
   SimpleGrid,
   useDisclosure,
   Text,
+  Button,
+  Center,
 } from '@chakra-ui/react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import ImgPost from './ImgPost';
 import { useDispatch, useSelector } from 'react-redux';
-import { getPosts, getUsers, getUser } from '../../redux/action';
+import { getUsers } from '../../redux/action';
 
-const ImgPostContainer = ({ site, myUser, email, user, posts, friendsPost }) => {
+const ImgPostContainer = ({
+  site,
+  myUser,
+  email,
+  user,
+  posts,
+  friendsPost,
+  singlePost,
+  handleClickRef,
+}) => {
+  const dispatch = useDispatch();
+  const { isOpen, onToggle } = useDisclosure();
 
-    const dispatch = useDispatch();
-    const { isOpen, onToggle } = useDisclosure();
-
-    const users = useSelector((state) => state.users);
-
+  const users = useSelector((state) => state.users);
 
   useEffect(() => {
     dispatch(getUsers());
   }, [dispatch]);
 
-
+  const handleClickMore = () => {
+    // setCurrentEnd(currentEnd + 8);
+  };
 
   const arrayUserPosts = (site) => {
     if (site === 'profile') {
@@ -32,35 +43,37 @@ const ImgPostContainer = ({ site, myUser, email, user, posts, friendsPost }) => 
     if (site === 'anyProfile') {
       return user;
     }
-    if (site === 'search' ) {
+    if (site === 'search') {
       return posts;
     }
-    if(site === 'feed'){
-      return friendsPost
+    if (site === 'feed') {
+      return friendsPost;
     }
   };
 
-if(site === 'feed' || site === 'search') {
-
-  return (
-    
-    <>
-      <Flex
-        pr={'2%'}
-        pl={'2%'}
-        textAlign={'center'}
-        justifyContent={'center'}
-        direction={'column'}
-        borderRadius={2}
-        mt={site === 'feed' ? '0vh' : '4vh'}
-        bg={'rgba(229, 191, 124, 0.3)'}
-      >
-        {
-          <SimpleGrid columns={{ base: 1, xl: 3 }} spacing={'10'} mt={2} mr={5}>
-            {arrayUserPosts(site).length !== 0 ? (
-              arrayUserPosts(site).map((post, index) => {
-                return (
-                  <SlideFade in={onToggle} key={index} offsetY="20px">
+  if (site === 'feed' || site === 'search') {
+    return (
+      <>
+        <Flex
+          pr={'2%'}
+          pl={'2%'}
+          textAlign={'center'}
+          justifyContent={'center'}
+          direction={'column'}
+          borderRadius={2}
+          mt={site === 'feed' ? '0vh' : '4vh'}
+          >
+          {
+            <SimpleGrid
+              columns={{ base: 1, xl: 3 }}
+              spacing={'10'}
+              mt={2}
+              mr={5}
+            >
+              {arrayUserPosts(site)?.length !== 0 ? (
+                arrayUserPosts(site)?.map((post, index) => {
+                  return (
+                    <SlideFade in={onToggle} key={index} offsetY="20px">
                       <ImgPost
                         userName={post.author?.userName}
                         fullName={post.author?.fullName}
@@ -69,39 +82,48 @@ if(site === 'feed' || site === 'search') {
                         avatar={post?.author?.image}
                         description={post?.description}
                         date={post?.createdAt}
+                        postId={post?._id}
+                        loggedUser={myUser?._id}
+                        loggedEmail={myUser?.email}
+                        singlePost={singlePost}
                       />
-                  </SlideFade>
-                );
-              })
-            ) : (
-              <Box>
-                <Text>no hay posteos</Text>{' '}
-              </Box>
-            )}
-          </SimpleGrid>
-        }
-      </Flex>
-    </>
-  );
-} else if(site === 'profile' || site === 'anyProfile'){
-  return (
-    <>
-      <Flex
-        pr={'2%'}
-        pl={'2%'}
-        textAlign={'center'}
-        justifyContent={'center'}
-        direction={'column'}
-        borderRadius={2}
-        mt={site === 'feed' ? '0vh' : '4vh'}
-        bg={'rgba(229, 191, 124, 0.3)'}
-      >
-        {
-          <SimpleGrid columns={{ base: 1, xl: 3 }} spacing={'10'} mt={2} mr={5}>
-            {arrayUserPosts(site)?.posts?.length !== 0 ? (
-              arrayUserPosts(site)?.posts?.map((post, index) => {
-                return (
-                  <SlideFade in={onToggle} key={index} offsetY="20px">
+                    </SlideFade>
+                  );
+                })
+              ) : (
+                <Box>
+                  <Text>no hay posteos</Text>{' '}
+                </Box>
+              )}
+            </SimpleGrid>
+          }
+        </Flex>
+      </>
+    );
+  } else if (site === 'profile' || site === 'anyProfile') {
+    return (
+      <>
+        <Flex
+          pr={'2%'}
+          pl={'2%'}
+          textAlign={'center'}
+          justifyContent={'center'}
+          direction={'column'}
+          borderRadius={2}
+          mt={site === 'feed' ? '0vh' : '4vh'}
+          bg={'rgba(229, 191, 124, 0.3)'}
+        >
+          {
+            <SimpleGrid
+              columns={{ base: 1, xl: 3 }}
+              spacing={'10'}
+              mt={2}
+              mr={5}
+            >
+              {arrayUserPosts(site)?.posts?.length !== 0 ? (
+                arrayUserPosts(site)?.posts?.map((post, index) => {
+                  return (
+                    <SlideFade in={onToggle} key={index} offsetY="20px">
                       <ImgPost
                         userName={arrayUserPosts(site)?.userName}
                         fullName={arrayUserPosts(site)?.fullName}
@@ -109,21 +131,45 @@ if(site === 'feed' || site === 'search') {
                         avatar={arrayUserPosts(site)?.image}
                         description={post.description}
                         date={post.createdAt}
+                        postId={post?._id}
+                        loggedUser={myUser?._id}
+                        loggedEmail={myUser?.email}
+                        singlePost={singlePost}
                       />
-                  </SlideFade>
-                );
-              })
-            ) : (
-              <Box>
-                <Text>no hay posteos</Text>{' '}
-              </Box>
-            )}
-          </SimpleGrid>
-        }
-      </Flex>
-    </>
-  );
-}
+                    </SlideFade>
+                  );
+                })
+              ) : (
+                <Box>
+                  {site === 'profile' ? (
+                    <Text w={'40vw'} ml={'15vw'}>
+                      You haven't create any posts. Click here to create your
+                      first one <Button onClick={handleClickRef}>Create</Button>
+                    </Text>
+                  ) : (
+                    <Text>This user has no posts yet</Text>
+                  )}
+                </Box>
+              )}
+            </SimpleGrid>
+          }
+          <Center>
+            <Button
+              onClick={() => handleClickMore()}
+              h="50px"
+              w="200px"
+              mr="50"
+              fontSize="sm"
+              mt="50px"
+              mb="50px"
+            >
+              Ver más
+            </Button>
+          </Center>
+        </Flex>
+      </>
+    );
+  }
 };
 
 export default ImgPostContainer;
--- client/src/components/TextPost/TextPostContainer.jsx
+++ client/src/components/TextPost/TextPostContainer.jsx
@@ -10,12 +10,20 @@ import {
   useDisclosure,
 } from '@chakra-ui/react';
 import TextPost from './TextPost';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../../redux/action';
 
-export default function TextPostContainer({ site, word, myUser, user, posts, friendsPost }) {
-
+export default function TextPostContainer({
+  site,
+  word,
+  myUser,
+  user,
+  posts,
+  friendsPost,
+  singlePost,
+  handleClickRef,
+}) {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getUsers());
@@ -23,7 +31,6 @@ export default function TextPostContainer({ site, word, myUser, user, posts, fri
 
   const { isOpen, onToggle } = useDisclosure();
   const users = useSelector((state) => state.users);
-
   const arrayUserPosts = (site) => {
     if (site === 'profile') {
       return myUser;
@@ -31,14 +38,13 @@ export default function TextPostContainer({ site, word, myUser, user, posts, fri
     if (site === 'anyProfile') {
       return user;
     }
-    if (site === 'search' ) {
+    if (site === 'search') {
       return posts;
     }
-    if(site === 'feed'){
-      return friendsPost
+    if (site === 'feed') {
+      return friendsPost;
     }
   };
-
   //--------- Lógica de ver mas --------
 
   // const [currentStart, setCurrentStart] = useState(0);
@@ -50,99 +56,116 @@ export default function TextPostContainer({ site, word, myUser, user, posts, fri
     // setCurrentEnd(currentEnd + 8);
   };
 
-  if(site === 'feed' || site === 'search') {
-
+  if (site === 'feed' || site === 'search') {
     return (
       <>
         <Flex
           pr={'2%'}
           pl={'2%'}
+          w={'100%'}
           textAlign={'center'}
           justifyContent={'center'}
           direction={'column'}
           borderRadius={2}
           mt={site === 'feed' ? '0vh' : '4vh'}
-          bg={'rgba(229, 191, 124, 0.3)'}
         >
-          { 
-            <SimpleGrid columns={{ base: 1, xl: 3 }} spacing={'10'} mt={2} mr={5}>
+          {
+            <SimpleGrid
+              columns={{ base: 1, xl: 3 }}
+              spacing={'10'}
+              mt={2}
+              mr={5}
+            >
               {arrayUserPosts(site)?.length !== 0 ? (
                 arrayUserPosts(site).map((post, index) => {
                   return (
                     <SlideFade in={onToggle} key={index} offsetY="20px">
-                        <TextPost
-                          userName={post.author?.userName}
-                          fullName={post.author?.fullName}
-                          image={post?.pics}
-                          email={post?.author?.email}
-                          avatar={post?.author?.image}
-                          description={post?.description}
-                          date={post?.createdAt}
-                        />
+                      <TextPost
+                        userName={post.author?.userName}
+                        fullName={post.author?.fullName}
+                        postId={post._id}
+                        singlePost={singlePost}
+                        image={post?.pics}
+                        email={post?.author?.email}
+                        avatar={post?.author?.image}
+                        description={post?.description}
+                        date={post?.createdAt}
+                        loggedUser={myUser?._id}
+                      />
                     </SlideFade>
                   );
                 })
               ) : (
                 <Box>
-                  <Text>no hay posteos</Text>{' '}
+                  <Text>Follow other users to see updates!</Text>{' '}
                 </Box>
               )}
-            </SimpleGrid> 
+            </SimpleGrid>
           }
         </Flex>
       </>
     );
-  } else if(site === 'profile' || site === 'anyProfile'){
-  return (
-    <Flex
-      pr={'2%'}
-      pl={'2%'}
-      textAlign={'center'}
-      justifyContent={'center'}
-      direction={'column'}
-      bg={'rgba(229, 191, 124, 0.2)'}
-      borderRadius={2}
-      mt={site === 'feed' ? '0vh' : '4vh'}
-    >
-      <SimpleGrid columns={{ base: 1, xl: 2 }} spacing={'10'} mt={2} mr={5}>
-        {arrayUserPosts(site)?.posts?.length !== 0 ? (
-          arrayUserPosts(site)?.posts?.map((post, index) => {
-            return (
-              <SlideFade in={onToggle} offsetY="20px">
-                <Box key={index}>
-                  <TextPost
-                    fullName={arrayUserPosts(site)?.fullName}
-                    image={arrayUserPosts(site)?.image}
-                    description={post.description}
-                    userName={arrayUserPosts(site)?.userName}
-                    background={`logo.${Math.random(1, 2, 3)}`}
-                  />
-                </Box>
-              </SlideFade>
-            );
-          })
-        ) : (
-          <Box>
-            <Text>no hay posteos</Text>{' '}
-          </Box>
-        )}
-      </SimpleGrid>
-      <Center>
-        <Button
-          onClick={() => handleClickMore()}
-          h="50px"
-          w="200px"
-          mr="50"
-          fontSize="sm"
-          mt="50px"
-          mb="50px"
-        >
-          Ver más
-        </Button>
-      </Center>
-    </Flex>
-  );
-}
+  } else if (site === 'profile' || site === 'anyProfile') {
+    return (
+      <Flex
+        pr={'2%'}
+        pl={'2%'}
+        textAlign={'center'}
+        justifyContent={'center'}
+        direction={'column'}
+        bg={'rgba(229, 191, 124, 0.2)'}
+        borderRadius={2}
+        mt={site === 'feed' ? '0vh' : '4vh'}
+      >
+        <SimpleGrid columns={{ base: 1, xl: 2 }} spacing={'10'} mt={2} mr={5}>
+          {arrayUserPosts(site)?.posts?.length !== 0 ? (
+            arrayUserPosts(site)?.posts?.map((post, index) => {
+              return (
+                <SlideFade in={onToggle} offsetY="20px">
+                  <Box key={index}>
+                    <TextPost
+                      singlePost={singlePost}
+                      postId={post._id}
+                      fullName={arrayUserPosts(site)?.fullName}
+                      image={arrayUserPosts(site)?.image}
+                      description={post.description}
+                      userName={arrayUserPosts(site)?.userName}
+                      loggedUser={myUser?._id}
+                      background={`logo.${Math.random(1, 2, 3)}`}
+                    />
+                  </Box>
+                </SlideFade>
+              );
+            })
+          ) : (
+            <Box>
+              {site === 'profile' ? (
+                <Text w={'40vw'} ml={'15vw'}>
+                  You haven't create any posts. Click here to create your first
+                  one <Button onClick={handleClickRef}>Create</Button>
+                </Text>
+              ) : (
+                <Text>This user has no posts yet</Text>
+              )}
+            </Box>
+          )}
+        </SimpleGrid>
+        <Center>
+          <Button
+            onClick={() => handleClickMore()}
+            h="50px"
+            w="200px"
+            mr="50"
+            fontSize="sm"
+            mt="50px"
+            mb="50px"
+          >
+            Ver más
+          </Button>
+        </Center>
+      </Flex>
+    );
+  }
 }
 // }
 
