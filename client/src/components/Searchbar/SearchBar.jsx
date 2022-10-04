f23b4f40ebbe89a96d2f92fb5c1760904f28d5b1
El Bebelo <nacho71197@gmail.com>
2022-10-04 20:27:40 -0300

    Arreglo de estados

--- client/src/components/Searchbar/SearchBar.jsx
+++ client/src/components/Searchbar/SearchBar.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Input, Box, IconButton } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { searchUser, searchPost } from '../../redux/action';
+import {
+  searchUser,
+  searchPost,
+  getMyUser,
+  getFriends,
+  getFollowers,
+  getFriendsPosts,
+} from '../../redux/action';
 
 const Searchbar = () => {
-
   const myUser = useSelector((state) => state.myUser);
   const friends = useSelector((state) => state.friends);
   const myFollowers = useSelector((state) => state.followers);
-
+  const [User, setUser] = useState(
+    useState(JSON.parse(localStorage.getItem('user')))
+  );
+  const neededEmail = User[0].email;
+  const neededId = User[0]._id;
+  console.log(!myUser?.email);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [searcher, setSearcher] = useState('');
@@ -19,16 +30,17 @@ const Searchbar = () => {
     e.preventDefault();
     setSearcher(e.target.value);
   }
-
+  if (!myUser?.email) {
+    dispatch(getMyUser(neededEmail));
+    dispatch(getFriends(neededId));
+    dispatch(getFollowers(neededId));
+    dispatch(getFriendsPosts(neededId));
+  }
   // useEffect(() => {
   //   dispatch(getPosts())
-  //   dispatch(getFriends(myUser?._id))
-  //   dispatch(getFollowers(myUser?._id))
-  //   dispatch(getFriendsPosts(myUser?._id));
   //   dispatch(getMyUser())
-    
+
   // }, [dispatch]);
-  
 
   function handleSubmit(e) {
     e.preventDefault();
